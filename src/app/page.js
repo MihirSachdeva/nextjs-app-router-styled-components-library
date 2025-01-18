3e97211e29381439b0b8a5acf59361d8ffa881ac
Mihir Sachdeva <mihirsachdeva.ms@gmail.com>
2025-01-18 14:49:05 +0530

    Add AComponent and styled-components integration with global styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { AComponent } from "@/components/AComponent";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <main className={`${styles.main} light`}>
         <Image
           className={styles.logo}
           src="/next.svg"
@@ -13,6 +14,9 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <AComponent />
+
         <ol>
           <li>
             Get started by editing <code>src/app/page.js</code>.
